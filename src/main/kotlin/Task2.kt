86633a899199044a47a47aef9b5bff560d162fae
VictorLesnoy <martynovv@list.ru>
2025-04-28 22:36:49 +0500

    KS-1-1 create smth

--- src/main/kotlin/Task2.kt
+++ src/main/kotlin/Task2.kt
@@ -1,6 +1,10 @@
 fun main() {
-    var quantityOfOrders = 75
-    val thanksForPurchase = "Спасибо за покупку"
+    var quantityOfOrders: Int = 75
+    val thanksForPurchase: String = "Спасибо за покупку"
     println ("Заказов на сегодня " + quantityOfOrders)
     println (thanksForPurchase)
+    var quantityOfEmployees: Int = 2000
+    println("Количество работников " + quantityOfEmployees + " человек")
+    quantityOfEmployees = quantityOfEmployees - 1
+    println("Количество работников стало " + quantityOfEmployees + " человек")
 }
