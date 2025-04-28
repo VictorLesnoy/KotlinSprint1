ab4642ba95810379f861002a92ac6d77fbb83119
VictorLesnoy <martynovv@list.ru>
2025-04-28 20:59:40 +0500

    KS-1-1 create smth

---
+++ src/main/kotlin/Lesson1/Lesson1_Task2.kt
@@ -0,0 +1,8 @@
+package Lesson1
+
+fun main() {
+    var quantityOfOrders = 75
+    val thanksForPurchase = "Спасибо за покупку"
+    println ("Заказов на сегодня " + quantityOfOrders)
+    println (thanksForPurchase)
+}
