5a049fddede904a2390ee39bd8f335fab27fee4f
VictorLesnoy <martynovv@list.ru>
2025-04-28 22:38:37 +0500

    KS-1-2

--- src/main/kotlin/Task2.kt
+++ src/main/kotlin/Task2.kt
@@ -1,6 +1,6 @@
 fun main() {
     var quantityOfOrders: Int = 75
-    val thanksForPurchase: String = "Спасибо за покупку"
+    val thanksForPurchase: String = "Спасибо за покупку!"
     println ("Заказов на сегодня " + quantityOfOrders)
     println (thanksForPurchase)
     var quantityOfEmployees: Int = 2000
