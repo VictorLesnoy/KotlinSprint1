d5716579e8a82cbf3b7dbcb3d4f323dfbb582ea2
VictorLesnoy <martynovv@list.ru>
2025-04-28 20:52:23 +0500

    KS-1-1 create smth

--- Lesson1_Task2.kt
+++ Lesson1_Task2.kt
@@ -3,5 +3,6 @@ package Lesson1
 fun main() {
     var quantityOfOrders = 75
     val thanksForPurchase = "Спасибо за покупку"
-    println()
+    println ("Заказов на сегодня" + quantityOfOrders)
+    println (thanksForPurchase)
 }
