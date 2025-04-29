77471267cccb56d095d6e4cdaa265f88e0cc5996
VictorLesnoy <martynovv@list.ru>
2025-04-29 10:18:01 +0500

    KS-1-3

---
+++ src/main/kotlin/Task3.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val year = 1961
+    var hour = "09"
+    var minute = "07"
+    println("Год полёта - " + year)
+    println("Час взлёта - " + hour)
+    println("Минуты взлёта - " + minute)
+    hour = "10"
+    minute = "55"
+    println("Посадка " + hour + ":" + minute)
+}
