6f16a2d3dd3c8dbac9c8687392416a4028449e2f
VictorLesnoy <martynovv@list.ru>
2025-05-01 13:53:30 +0500

    Second

--- src/main/kotlin/Task 2.kt
+++ src/main/kotlin/Task 2.kt
@@ -1,12 +1,12 @@
 fun main() {
     val workers = 50
     val workerSalary = 30000
-    val intern = 30
+    val interns = 30
     val internSalary = 20000
     val totalWorkerSalary = workers * workerSalary
-    val totalSalary = (workers * workerSalary) + (intern * internSalary)
-    val averageSalary = totalSalary / (workers + intern)
-    println("Расходы на выплату зарплаты постоянных сотрудников: " + totalWorkerSalary)
-    println("Общие расходы по ЗП после прихода стажеров: " + totalSalary)
-    println("Среднюю ЗП одного сотрудника после устройства стажеров: " + averageSalary)
+    val totalSalary = (workers * workerSalary) + (interns * internSalary)
+    val averageSalary = totalSalary / (workers + interns)
+    println("Расходы на выплату зарплаты постоянных сотрудников: $totalWorkerSalary")
+    println("Общие расходы по ЗП после прихода стажеров: $totalSalary")
+    println("Среднюю ЗП одного сотрудника после устройства стажеров: $averageSalary")
 }
