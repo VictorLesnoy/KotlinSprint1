623ea7e78464a0e705403fa11cd091def74c4019
VictorLesnoy <martynovv@list.ru>
2025-05-03 22:24:37 +0500

    KS-2-4

---
+++ src/main/kotlin/Task 4.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val crystalOre: Int = 7
+    val ironOre = 11
+    val buff = 0.2
+
+    println("Бафф кристалической руды " + (crystalOre * buff).toInt())
+    println("Бафф железной руды " + (ironOre * buff).toInt())
+}
